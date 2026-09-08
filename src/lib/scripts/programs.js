703e01bb6f0eee073c838ae0459ce7ce0d6afc23
ianheuser <102179890+ianheuser@users.noreply.github.com>
2026-09-08 14:52:31 -0500

    remove bullet

--- src/lib/scripts/programs.js
+++ src/lib/scripts/programs.js
@@ -34,7 +34,7 @@ export const programs = [
         
         careerPathHeader: "ONE DEGREE. MULTIPLE CAREER PATHS.",
         careerPathBullets: [
-            "Entry Level Data Analyst" , "<span class='needs-approval'>Client Add Another Career?</span>"
+            "<span class='needs-approval'>Entry Level Data Analyst</span>"
         ],
         
         statisticsBar: [
